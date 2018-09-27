62fbd6f34f2d56aeec913da78a7e0c78f3c5bdfb
cyu8110 <cyu8110@gmail.com>
2018-09-27 20:56:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ function currentLine (katzDeliLine) {
   } else {
     for (let i = 0; i < katzDeliLine.length; i++) {
       list.push (i+1 + ". " + katzDeliLine[i]);
-      list.join (", ");
       }
+      list.join (", ");
     }
     return "The line is currently: " + list;
 }
