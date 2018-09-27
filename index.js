b327d1353a760a8df1f65560cb67ffc4bed73733
cyu8110 <cyu8110@gmail.com>
2018-09-27 20:17:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,13 +17,13 @@ function nowServing (katzDeliLine) {
 }
 
 function currentLine (katzDeliLine) {
-  var list;
+  var list = [];
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } else {
     for (let i = 0; i < katzDeliLine.length; i++) {
       list.push (i + "." + katzDeliLine[i] + ", ");
     }
-    console.log("The line is currently " + list);
+    return "The line is currently " + list;
   }
 }
