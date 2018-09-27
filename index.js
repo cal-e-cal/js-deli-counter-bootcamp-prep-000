443808b4bdebfc5d66a4901420e6c0f973c02c17
cyu8110 <cyu8110@gmail.com>
2018-09-27 20:24:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ function currentLine (katzDeliLine) {
   if (katzDeliLine.length === 0) {
     return "The line is currently empty.";
   } else {
-    for (let i = 0; i < katzDeliLine.length-1; i++) {
+    for (let i = 0; i < katzDeliLine.length; i++) {
       list.push (i+1 + ". " + katzDeliLine[i] + " ");
     }
-    return "The line is currently " + list;
+    return "The line is currently: " + list;
   }
 }
